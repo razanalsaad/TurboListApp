808a668f8fdc8b4a9eba4880e65b184066935f4f
razanalsaad <147040760+razanalsaad@users.noreply.github.com>
2024-10-17 11:47:24 +0300

    Initial Commit

---
+++ TurboList/TurboListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurboListApp.swift
+//  TurboList
+//
+//  Created by razan on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TurboListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
