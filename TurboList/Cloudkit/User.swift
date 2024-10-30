7a35ffd4033cd6b9f877c1b57b3de7b707846779
rahafibrahim21 <rahafibrahim999@gmail.com>
2024-10-30 21:05:18 +0300

    sign in with apple - done language setting - done log out functionality - done

---
+++ TurboList/Cloudkit/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  TurboList
+//
+//  Created by Rahaf ALghuraibi on 27/04/1446 AH.
+//
+
+import SwiftUI
+
+struct User: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    User()
+}
